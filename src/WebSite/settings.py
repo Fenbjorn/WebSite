8cf5d72c1674e10a5608b7c5264f68547a58d08a
Fenbjorn <ngrenoux@gmail.com>
2021-10-06 22:32:54 +0200

    Host

--- src/WebSite/settings.py
+++ src/WebSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cf+tq5a$)jxsbq#au3a1ogh1s&7)t$z758r9e_)(p7cpb804h8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Fenbjorn.pythonanywhere.com']
 
 
 # Application definition
