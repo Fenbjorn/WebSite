731a588a07099b0b28df4860960249b6adefbace
Fenbjorn <61088316+Fenbjorn@users.noreply.github.com>
2021-10-07 22:15:38 +0200

    Update settings.py

--- src/WebSite/settings.py
+++ src/WebSite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'home/Fenbjorn/WebSite/static'
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'WebSite/static')
 #]
